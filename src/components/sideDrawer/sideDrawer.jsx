31ed1ec85641ac86e7b77e4bc4edb09c8ab68025
MSHNK1 <nikaqochqiani@yaho..com>
2023-02-13 04:01:56 +0400

    backDrop fixed

--- src/components/sideDrawer/sideDrawer.jsx
+++ src/components/sideDrawer/sideDrawer.jsx
@@ -5,15 +5,13 @@ import './sideDrawer.css';
 
 export default function SideDrawer(props) {
   let attachedClasses = "";
-  let backDrop_true ="false"
   if (props.opened) {
-    backDrop_true = "true"
     attachedClasses = "open"
   }
   return (
     <div>
-      <BackDrop show={backDrop_true} clicked={props.closed}/>
-      <div className={`sideDrawer ${attachedClasses} mobile-xl:hidden`} show={backDrop_true} onClick={props.closed}>
+      <BackDrop show={props.opened} clicked={props.closed}/>
+      <div className={`sideDrawer ${attachedClasses} mobile-xl:hidden`} show={props.opened} onClick={props.closed}>
           <NavigationItems />
       </div>
     </div>
